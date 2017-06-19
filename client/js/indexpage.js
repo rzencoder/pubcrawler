51dd5886908921f8d00b6c423655a0c6e0accd1d
rzencoder <hamer88@hotmail.co.uk>
2017-06-19 10:32:36 +0100

    added comments

--- client/js/indexpage.js
+++ client/js/indexpage.js
@@ -89,7 +89,7 @@ $("#search").bind('submit', search);
 
 var js_file = document.createElement('script');
 js_file.type = 'text/javascript';
-js_file.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCGFWV94drOwU9IeHY_aVdiZcmWyYr4s8A&callback=initMap";
+js_file.src = "https://maps.googleapis.com/maps/api/js?key=API_KEY&callback=initMap";
 document.getElementsByTagName('head')[0].appendChild(js_file);
 
 var mapStyle = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}]
